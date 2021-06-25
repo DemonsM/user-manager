2812e0bd7f88483e2629a8afff5011c9f73140c7
梅涛 <573945966@qq.com>
2021-06-25 10:33:18 +0800

    update init.sql

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,9 +1,18 @@
-CREATE TABLE t_user
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user`
 (
-    id     BIGINT(20) PRIMARY KEY,
-    name   VARCHAR(20),
-    age    INT(3),
-    salary DOUBLE(7, 2
-) ,
-    phone INT(11)
-)
+    `id`     bigint(20) NOT NULL AUTO_INCREMENT,
+    `name`   varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+    `age`    int(3) NULL DEFAULT NULL,
+    `salary` double(7, 2) NULL DEFAULT NULL,
+  `phone` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
